28f84c8f4b5ec6c1c91450ecf9310679adeeff03
Vahid Dabbagh <dabbaghvahid@gmail.com>
2025-07-22 13:55:09 -0400

    adding MakeModel and MakeCar models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,12 +1,14 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+admin.site.register(CarMake)
 
 # CarModelAdmin class
+admin.site.register(CarModel)
 
 # CarMakeAdmin class with CarModelInline
 
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
 
     path(route='logout', view=views.logout_request, name='login'),
 
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
     # path for dealer reviews view
 
     # path for add a review view
