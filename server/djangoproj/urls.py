c157228f99d33a6053166b4d0c41c9e0737b912c
Vahid Dabbagh <dabbaghvahid@gmail.com>
2025-07-22 08:59:21 -0400

    adding login, logout and registration

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,11 +18,14 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
-        path('about/', TemplateView.as_view(template_name="About.html")),
-        path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
