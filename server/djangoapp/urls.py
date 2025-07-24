64015106ffe1311568d7278118c9dee7400a432f
Vahid Dabbagh <dabbaghvahid@gmail.com>
2025-07-24 10:56:33 -0400

    fix register route

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,7 @@ from .restapis import get_request, analyze_review_sentiments, post_review
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-    path(route='registration', view=views.registration, name='registration'),
+    path(route='register', view=views.registration, name='registration'),
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
